edee76a85d2dd7058abdbc9057f18ef381c4320a
Joshua Liu <joshua.liu@sourceobby.com>
2025-07-12 11:36:08 -0400

    15

--- ttt_package/bot/python/generator.py
+++ ttt_package/bot/python/generator.py
@@ -1,3 +1,4 @@
+import threading
 """
 # player true means x
 """
@@ -32,6 +33,8 @@ class Board:
 class Evaluation:
     def __init__(self):
         self.turns = 1
+        self.evallock = threading.Lock()
+        self.eval = 0.5
 
     def print_gameboard(self, gameboard):
         for i in range (10):
@@ -43,29 +46,25 @@ class Evaluation:
             return 0
         return a
 
-    def shit2(self, gameboard, penalty):
-        self.turns += 1
-        turn = self.turns // 2
-        horizontalwin = 0b1111100000
-        verticalwin = 0b1000000000
-        lrdiagonalwin = [0b1000000000, 0b0100000000, 0b0010000000, 0b0001000000, 0b0000100000, 0b0000010000,
-                         0b0000001000, 0b0000000100, 0b0000000010, 0b0000000001]
-        rldiagonalwin = [0b0000000001, 0b0000000010, 0b0000000100, 0b0000001000, 0b0000010000, 0b0000100000,
-                         0b0001000000, 0b0010000000, 0b0100000000, 0b1000000000]
-        closewins = [0b0111100000, 0b0011110000, 0b0001111000, 0b0000111100, 0b0000011110]
-        eval = 0.5 - penalty
-        # Check for horizontal win
-        hw = horizontalwin
+    def check_horiz(self, gameboard, turn, eval):
+        hw = 0b1111100000
         for i in range(5):
             check = [a & hw for a in gameboard]
             for row in check:
                 if row == hw:
-                    return float('inf')
+                    self.evallock.acquire(blocking=True, timeout=-1)
+                    self.eval += float('inf')
+                    self.evallock.release()
                 rowcount = bin(row).count('1')
                 if (rowcount > 1):
-                    eval += self.evaluate(rowcount, turn)
+                    self.evallock.acquire(blocking=True, timeout=-1)
+                    self.eval += self.evaluate(rowcount, turn)
+                    self.evallock.release()
             hw = hw >> 1
-        # Check for vertical win
+        return 0
+
+    def check_vert(self, gameboard, turn, eval):
+        verticalwin = 0b1000000000
         vw = verticalwin
         for i in range(10):
             wincount = 0
@@ -75,14 +74,20 @@ class Evaluation:
                     wincount += 1
                 else:
                     if (wincount != 1):
-                        eval += self.evaluate(wincount, turn)
+                        self.evallock.acquire(blocking=True, timeout=-1)
+                        self.eval += self.evaluate(wincount, turn)
+                        self.evallock.release()
                     wincount = 0
                 if (wincount == 5):
-                    return float('inf') # we have a win
+                    self.evallock.acquire(blocking=True, timeout=-1)
+                    self.eval += float('inf')  # we have a win
+                    self.evallock.release()
             vw = vw >> 1
-        # Check for diagonal win
-        # first check top to bottom diagonal moving left to right
-        dw1 = lrdiagonalwin
+        return 0
+
+    def check_diagonal1(self, gameboard, turn, eval):
+        dw1 = [0b1000000000, 0b0100000000, 0b0010000000, 0b0001000000, 0b0000100000, 0b0000010000,
+                         0b0000001000, 0b0000000100, 0b0000000010, 0b0000000001]
         for i in range(5):
             wincount = 0
             check = [a & b for a, b in zip(gameboard, dw1)]
@@ -92,15 +97,22 @@ class Evaluation:
                     wincount += 1
                 else:
                     if (wincount != 1):
-                        eval += self.evaluate(wincount, turn)
+                        self.evallock.acquire(blocking=True, timeout=-1)
+                        self.eval += self.evaluate(wincount, turn)
+                        self.evallock.release()
                     wincount = 0
                 if (wincount == 5):
-                    return float('inf') # we have a win
+                    self.evallock.acquire(blocking=True, timeout=-1)
+                    self.eval += float('inf')  # we have a win
+                    self.evallock.release()
                 z += 1
             for z in range(10):
                 dw1[z] = dw1[z] >> 1
-        # second check right to left
-        dw1 = lrdiagonalwin
+        return 0
+
+    def check_diagonal2(self, gameboard, turn, eval):
+        dw1 = [0b1000000000, 0b0100000000, 0b0010000000, 0b0001000000, 0b0000100000, 0b0000010000,
+                         0b0000001000, 0b0000000100, 0b0000000010, 0b0000000001]
         for i in range(5):
             wincount = 0
             check = [a & b for a, b in zip(gameboard, dw1)]
@@ -110,56 +122,121 @@ class Evaluation:
                     wincount += 1
                 else:
                     if (wincount != 1):
-                        eval += self.evaluate(wincount, turn)
+                        self.evallock.acquire(blocking=True, timeout=-1)
+                        self.eval += self.evaluate(wincount, turn)
+                        self.evallock.release()
                     wincount = 0
                 if (wincount == 5):
-                    return float('inf')  # we have a win
+                    self.evallock.acquire(blocking=True, timeout=-1)
+                    self.eval += float('inf')  # we have a win
+                    self.evallock.release()
                 z += 1
             for z in range(10):
                 dw1[z] = dw1[z] << 1
+        return 0
+
+    def check_diagonal3(self, gameboard, turn, eval):
+        dw1 = [0b0000000001, 0b0000000010, 0b0000000100, 0b0000001000, 0b0000010000, 0b0000100000,
+               0b0001000000, 0b0010000000, 0b0100000000, 0b1000000000]
+        for i in range(5):
+            wincount = 0
+            check = [a & b for a, b in zip(gameboard, dw1)]
+            z = 0
+            for row in check:
+                if (row == dw1[z]) and (row != 0):
+                    wincount += 1
+                else:
+                    if (wincount != 1):
+                        self.evallock.acquire(blocking=True, timeout=-1)
+                        self.eval += self.evaluate(wincount, turn)
+                        self.evallock.release()
+                    wincount = 0
+                if (wincount == 5):
+                    self.evallock.acquire(blocking=True, timeout=-1)
+                    self.eval += float('inf')  # we have a win
+                    self.evallock.release()
+                z += 1
+            for z in range(10):
+                dw1[z] = dw1[z] >> 1
+        return 0
+    def check_diagonal4(self, gameboard, turn, eval):
+        dw1 = [0b0000000001, 0b0000000010, 0b0000000100, 0b0000001000, 0b0000010000, 0b0000100000,
+               0b0001000000, 0b0010000000, 0b0100000000, 0b1000000000]
+        for i in range(5):
+            wincount = 0
+            check = [a & b for a, b in zip(gameboard, dw1)]
+            z = 0
+            for row in check:
+                if (row == dw1[z]) and (row != 0):
+                    wincount += 1
+                else:
+                    if (wincount != 1):
+                        self.evallock.acquire(blocking=True, timeout=-1)
+                        self.eval += self.evaluate(wincount, turn)
+                        self.evallock.release()
+                    wincount = 0
+                if (wincount == 5):
+                    self.evallock.acquire(blocking=True, timeout=-1)
+                    self.eval += float('inf')  # we have a win
+                    self.evallock.release()
+                z += 1
+            for z in range(10):
+                dw1[z] = dw1[z] << 1
+        return 0
+
+    def shit2(self, gameboard, penalty):
+        self.turns += 1
+        turn = self.turns // 2
+
+
+        rldiagonalwin = [0b0000000001, 0b0000000010, 0b0000000100, 0b0000001000, 0b0000010000, 0b0000100000,
+                         0b0001000000, 0b0010000000, 0b0100000000, 0b1000000000]
+        closewins = [0b0111100000, 0b0011110000, 0b0001111000, 0b0000111100, 0b0000011110]
+        self.eval = 0.5
+        threads = []
+        t = threading.Thread(target=self.check_horiz, args=(gameboard, turn, self.eval))
+        threads.append(t)
+        t = threading.Thread(target=self.check_vert, args=(gameboard, turn, self.eval))
+        threads.append(t)
+        t = threading.Thread(target=self.check_diagonal1, args=(gameboard, turn, self.eval))
+        threads.append(t)
+        t = threading.Thread(target=self.check_diagonal2, args=(gameboard, turn, self.eval))
+        threads.append(t)
+        t = threading.Thread(target=self.check_diagonal3, args=(gameboard, turn, self.eval))
+        threads.append(t)
+        t = threading.Thread(target=self.check_diagonal4, args=(gameboard, turn, self.eval))
+        threads.append(t)
+        # Start each thread
+        for t in threads:
+            t.start()
+
+        # Wait for all threads to finish
+        for t in threads:
+            t.join()
+
+        return self.eval
+        # t = threading.Thread(target=self.check_horiz, args=(gameboard, turn, eval))
+        # threads.append(t)
+        # t = threading.Thread(target=self.check_horiz, args=(gameboard, turn, eval))
+        # threads.append(t)
+        # Check for horizontal win
+
+        # Check for vertical win
+
+        # Check for diagonal win
+        # first check top to bottom diagonal moving left to right
+
+        # second check right to left
 
         # now we check with a bottom to top diagonal
         # first check moving left to right
-        dw1 = rldiagonalwin
-        for i in range(5):
-            wincount = 0
-            check = [a & b for a, b in zip(gameboard, dw1)]
-            z = 0
-            for row in check:
-                if (row == dw1[z]) and (row != 0):
-                    wincount += 1
-                else:
-                    if (wincount != 1):
-                        eval += self.evaluate(wincount, turn)
-                    wincount = 0
-                if (wincount == 5):
-                    return float('inf')  # we have a win
-                z += 1
-            for z in range(10):
-                dw1[z] = dw1[z] >> 1
+
         # second check right to left
-        dw1 = rldiagonalwin
-        for i in range(5):
-            wincount = 0
-            check = [a & b for a, b in zip(gameboard, dw1)]
-            z = 0
-            for row in check:
-                if (row == dw1[z]) and (row != 0):
-                    wincount += 1
-                else:
-                    if (wincount != 1):
-                        eval += self.evaluate(wincount, turn)
-                    wincount = 0
-                if (wincount == 5):
-                    return float('inf')  # we have a win
-                z += 1
-            for z in range(10):
-                dw1[z] = dw1[z] << 1
         return eval
 
 b = Evaluation()
 # [0b0000000000, 0b0000000000, 0b0000000000, 0b0000000000, 0b0000000000, 0b0000000000,
 #                          0b0000000000, 0b0000000000, 0b0000000000, 0b0000000000]
-g  = [0b0000000000, 0b0000000000, 0b0010000000, 0b0001000000, 0b0000100000, 0b0000010000,
+g  = [0b0000000000, 0b0000000000, 0b0010000000, 0b0001000000, 0b0000100000, 0b0000000000,
                          0b0000001000, 0b0000000000, 0b0000000000, 0b0000000000]
 print(b.shit2(g, 0))
